4398ca276983a44912cb19364bf2afa18064bfd1
AnandManit09 <anand8manit@gmail.com>
2024-10-31 12:37:53 +0530

    Sample files update

---
+++ Bit Manipulation/SampleFile.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    // Function to calculate maximum profit
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Number of Jobs: " + result[1]);
+        System.out.println("Maximum Profit: " + result[0]);
+    }
+}
---
+++ Heaps/sampleFile.java
@@ -0,0 +1,8 @@
+class Solution {
+
+    public static void main(String[] args) {
+
+        System.out.println("Number of Jobs: " + result[1]);
+        System.out.println("Maximum Profit: " + result[0]);
+    }
+}
